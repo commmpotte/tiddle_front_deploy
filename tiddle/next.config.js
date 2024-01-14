1dbfec39b0f063ece9b64ffdf25f652f4196bbfd
Aleksandr <smirnow-2012@mail.ru>
2024-01-14 13:19:26 +0300

    initial sass config

--- tiddle/next.config.js
+++ tiddle/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = (phase, { defaultConfig }) => {
+	if ('sassOptions' in defaultConfig) {
+		defaultConfig['sassOptions'] = {
+			includePaths: ['./src'],
+			prependData: `@import "~@styles/_variables.scss";`
+		};
+	}
+	return {
+		serverRuntimeConfig: {
+			// ...
+			cookieSecret: process.env.NEXT_SECRET_COOKIE // ваш секрет для кук
+		},
+		...defaultConfig
+	};
+};
