a50b9da96cc1fe5e06fdde670db253ce8c8d4e40
Aleksandr <smirnow-2012@mail.ru>
2024-01-15 22:27:29 +0300

    add middlaware and routing auth

---
+++ tiddle/src/configs/auth.config.ts
@@ -0,0 +1,27 @@
+import { AuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export const authConfig: AuthOptions = {
+	providers: [
+		CredentialsProvider({
+			credentials: {
+				identifier: { label: 'identifier', type: 'identifier' },
+				password: { label: 'password', type: 'password' }
+			},
+			async authorize(credentials) {
+				return null;
+			}
+		})
+	],
+	pages: {
+		signIn: '/login'
+	},
+	callbacks: {
+		async jwt({ token, user }) {
+			return { ...token, ...user };
+		},
+		async session({ session }) {
+			return session;
+		}
+	}
+};
