c4ac5cba20cdc670b85309fd4dffc489ceeb1c3b
Aleksandr <smirnow-2012@mail.ru>
2024-02-12 20:05:30 +0300

    класс для получения API и переписал сами api

---
+++ tiddle/src/api/api.fetch.ts
@@ -0,0 +1,94 @@
+class FetchClient {
+	private API_URL = process.env.NEXT_PUBLIC_DJANGO_HOST as string;
+
+	constructor(private defaultHeaders: Record<string, string> = {}) {}
+
+	async get<T>(
+		path: string,
+		isAuth: boolean = false,
+		headers?: Record<string, string>,
+		token: string = ''
+	): Promise<T> {
+		return this.fetch<T>(path, 'GET', isAuth, undefined, headers, token);
+	}
+
+	async post<T>(
+		path: string,
+		body?: Record<string, any>,
+		isAuth: boolean = false,
+		headers?: Record<string, string>,
+		token: string = ''
+	): Promise<T> {
+		return this.fetch<T>(path, 'POST', isAuth, body, headers, token);
+	}
+
+	async put<T>(
+		path: string,
+		body?: Record<string, any>,
+		isAuth: boolean = false,
+		headers?: Record<string, string>,
+		token: string = ''
+	): Promise<T> {
+		return this.fetch<T>(path, 'PUT', isAuth, body, headers, token);
+	}
+
+	async delete<T>(
+		path: string,
+		isAuth: boolean = false,
+		headers?: Record<string, string>,
+		token: string = ''
+	): Promise<T> {
+		return this.fetch<T>(path, 'DELETE', isAuth, undefined, headers, token);
+	}
+
+	async patch<T>(
+		path: string,
+		body?: Record<string, any>,
+		isAuth: boolean = false,
+		headers?: Record<string, string>,
+		token: string = ''
+	): Promise<T> {
+		return this.fetch<T>(path, 'PATCH', isAuth, body, headers, token);
+	}
+
+	private async fetch<T>(
+		path: string,
+		method: string,
+		isAuth: boolean,
+		body?: Record<string, any>,
+		headers?: Record<string, string>,
+		token?: string
+	): Promise<T> {
+		const url = `${this.API_URL}${path}`;
+
+		const authorizationHeader: HeadersInit = isAuth
+			? { Authorization: `Bearer ${token}` }
+			: {};
+
+		// eslint-disable-next-line no-useless-catch
+		try {
+			const response = await fetch(url, {
+				method,
+				headers: {
+					'Content-Type': 'application/json',
+					...this.defaultHeaders,
+					...authorizationHeader,
+					...headers
+				},
+				body: body ? JSON.stringify(body) : null
+			});
+
+			const data = await response.json();
+			console.log(data);
+			if (!response.ok) {
+				throw new Error(data.error);
+			}
+
+			return data;
+		} catch (error) {
+			throw error;
+		}
+	}
+}
+
+export const $fetch = new FetchClient();
--- tiddle/src/api/createUser.api.ts
+++ tiddle/src/api/createUser.api.ts
@@ -1,29 +1,17 @@
+import { $fetch } from '@/api/api.fetch';
 import { pathAPI } from '@/helps/pathAPI.help';
-import { isApiError } from '@/types/interfaces/error.interface';
 import { ILogin } from '@/types/interfaces/login.interface';
 
 export async function createUser(dataCreateUser: ILogin) {
 	try {
-		console.log(process.env.NEXT_PUBLIC_DJANGO_HOST + pathAPI.auth.login);
-		const data = await fetch(
-			process.env.NEXT_PUBLIC_DJANGO_HOST + pathAPI.auth.createUser,
-			{
-				method: 'POST',
-				body: JSON.stringify(dataCreateUser),
-				headers: {
-					'Content-Type': 'application/json'
-				}
-			}
+		const data = await $fetch.post<ILogin>(
+			pathAPI.auth.createUser,
+			dataCreateUser
 		);
-		const response: ILogin = await data.json();
-		if (!data.ok) {
-			throw new Error(data.statusText);
-		}
-		return response;
-	} catch (error) {
-		if (isApiError(error)) {
-			throw new Error(`${error.message}`);
-		}
-		throw error;
+		return data;
+	} catch (e) {
+		return Promise.reject({
+			message: 'Register error, not valid data!'
+		});
 	}
 }
