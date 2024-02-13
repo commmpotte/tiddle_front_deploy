e28ef375452d23328d5278e1aa4dc8dd5f802faf
Aleksandr <smirnow-2012@mail.ru>
2024-02-13 15:56:12 +0300

    сделал кастомный скролл

--- tiddle/src/app/(landing)/page.tsx
+++ tiddle/src/app/(landing)/page.tsx
@@ -10,50 +10,59 @@ import { Plans } from './_components/Plans/Plans';
 import { Tag } from './_components/Tag/Tag';
 import { Faq } from './_components/Faq/Faq';
 import { Contact } from './_components/Contact/Contact';
+import { motion, useScroll, useSpring } from 'framer-motion';
 
 export default function LandingPage() {
+	const { scrollYProgress } = useScroll();
+	const scaleX = useSpring(scrollYProgress);
 	return (
-		<div className={styles.main}>
-			<div className={styles.wrapper}>
-				<Header texts={landingHelp.header}></Header>
-				<WhatIsIt
-					tag={landingHelp.title.tag}
-					description={landingHelp.title.description}
-					animateText={landingHelp.title.animateText}
-					textLink={landingHelp.title.textLink}
-				/>
-				<Troubles
-					tagH2={landingHelp.trobles.h2}
-					tagH3={landingHelp.trobles.h3}
-					secondaryH2={landingHelp.trobles.secondaryTextH2}
-					secondaryH3={landingHelp.trobles.secondaryTextH3}
-					slips={landingHelp.trobles.slips}
-				/>
-				<ElipseAvatars
-					tag={landingHelp.whyTiddle.tag}
-					slips={landingHelp.whyTiddle.text}
-				/>
-				<Table tag={landingHelp.table.tag} column={landingHelp.table.columns} />
-			</div>
-			<div className={styles.hditWrapper}>
+		<>
+			<motion.div className={styles.progressBar} style={{ scaleX }} />
+			<div className={styles.main}>
 				<div className={styles.wrapper}>
-					<div className={styles.tests} id="third-section">
-						<Tag className={styles.tag}>How does it work</Tag>
-						<img src="/landing/plug.png" alt="" className={styles.quiz} />
-						<div className={styles.iphone}>
-							<img src="/landing/iphone1.png" alt="" />
+					<Header texts={landingHelp.header}></Header>
+					<WhatIsIt
+						tag={landingHelp.title.tag}
+						description={landingHelp.title.description}
+						animateText={landingHelp.title.animateText}
+						textLink={landingHelp.title.textLink}
+					/>
+					<Troubles
+						tagH2={landingHelp.trobles.h2}
+						tagH3={landingHelp.trobles.h3}
+						secondaryH2={landingHelp.trobles.secondaryTextH2}
+						secondaryH3={landingHelp.trobles.secondaryTextH3}
+						slips={landingHelp.trobles.slips}
+					/>
+					<ElipseAvatars
+						tag={landingHelp.whyTiddle.tag}
+						slips={landingHelp.whyTiddle.text}
+					/>
+					<Table
+						tag={landingHelp.table.tag}
+						column={landingHelp.table.columns}
+					/>
+				</div>
+				<div className={styles.hditWrapper}>
+					<div className={styles.wrapper}>
+						<div className={styles.tests} id="third-section">
+							<Tag className={styles.tag}>How does it work</Tag>
+							<img src="/landing/plug.png" alt="" className={styles.quiz} />
+							<div className={styles.iphone}>
+								<img src="/landing/iphone1.png" alt="" />
+							</div>
 						</div>
 					</div>
 				</div>
+				<div className={styles.wrapperBottom}>
+					<Plans tag={landingHelp.plans.tag} cards={landingHelp.plans.card} />
+					<Faq cards={landingHelp.faq} />
+					<Contact
+						links={landingHelp.contact.links}
+						social={landingHelp.contact.social}
+					/>
+				</div>
 			</div>
-			<div className={styles.wrapperBottom}>
-				<Plans tag={landingHelp.plans.tag} cards={landingHelp.plans.card} />
-				<Faq cards={landingHelp.faq} />
-				<Contact
-					links={landingHelp.contact.links}
-					social={landingHelp.contact.social}
-				/>
-			</div>
-		</div>
+		</>
 	);
 }
